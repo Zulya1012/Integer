a07f3c8d085f0ec535a1a3911b2407bb9d68ba17
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-05 17:03:06 +0400

    Update fact1.java

--- fact1.java
+++ fact1.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
-public class fact1{
-  public static void main(String[] arg){
-    Scanner s=new Scanner(System.in);
+public class fact1 {
+  public static void main(String[] arg) {
+    Scanner s = new Scanner(System.in);
     System.out.print("Enter the integer");
-    int a=s.nextInt();
+    int a = s.nextInt();
     System.out.print(a);
-    
-    int count=0;
-    for (int i=1; i<=a; i++){
-      if (a%i==0){
+
+    int count = 0;
+    for (int i = 1; i <= a; i++) {
+      if (a % i == 0) {
         System.out.println(i);
-      count++;
+        count++;
       }
     }
-    System.out.print ("Total:" + " "+count);
+    System.out.print("Total:" + " " + count);
   }
 }
-    
